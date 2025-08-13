cac9f89460cb88d4301da9546e86bf36f8aa5d7b
PROFITESTRATEGISTA <PROFITESTRATEGISTA@GMAIL.COM>
2025-08-12 23:42:16 -0300

    Fix JSX closing tag error

--- src/components/admin/AdminLeadsPanel.tsx
+++ src/components/admin/AdminLeadsPanel.tsx
@@ -644,7 +644,6 @@ const AdminLeadsPanel: React.FC = () => {
                     className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     placeholder="Digite sua mensagem personalizada..."
                   />
-                  />
                 </div>
               )}
 
