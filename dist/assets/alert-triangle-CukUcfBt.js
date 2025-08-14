b4f6cef0e05754ff8eca9ffc47d65b917202f5a7
PROFITESTRATEGISTA <PROFITESTRATEGISTA@GMAIL.COM>
2025-08-14 02:06:53 -0300

    Publish application

--- dist/assets/alert-triangle-CukUcfBt.js
+++ dist/assets/alert-triangle-CukUcfBt.js
@@ -1,4 +1,4 @@
-import{c as e}from"./Layout-Bt_9Vxv_.js";/**
+import{c as e}from"./Layout-ZupUeJC8.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
