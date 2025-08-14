712e4f67f4f6ff78662191e828cc5e6912a3a9f0
PROFITESTRATEGISTA <PROFITESTRATEGISTA@GMAIL.COM>
2025-08-14 02:10:15 -0300

    Remove maximum drawdown field

--- src/pages/ResultadosPage.tsx
+++ src/pages/ResultadosPage.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import ResultsCalendar from '../components/ResultsCalendar';
 
 const ResultadosPage: React.FC = () => {
+  const [riskSettings, setRiskSettings] = useState({
+    dailyLossLimit: 5,
+    monthlyLossLimit: 15,
+    stopLossPercentage: 2,
+    isActive: true
+  });
+
   return (
     <Layout>
       <ResultsCalendar />
